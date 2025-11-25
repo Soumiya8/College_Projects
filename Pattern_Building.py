96898793c71b846339634ba74cd93de1dad107d5
Soumiya S <soumiyasankar4s@gmail.com>
2025-11-25 19:16:45 +0530

    learnt pattern building

---
+++ Pattern_Building.py
@@ -0,0 +1,27 @@
+# pattern
+#Right angle triangle
+'''n = int(input("Enter the no. of rows"))
+for i in range (1,n+1) :
+    print("*" * i)'''
+    
+#TRIANGLE diamond
+'''n = int(input("Enter the no. of rows"))
+for i in range (1,n+1) :
+    if i == 1:
+        print(" " * (n-i) + "*")
+    else:
+        print(" " * (n-i) + "*" + "*" * (2*i - 2))
+
+for i in range (n-1,0,-1) :
+    if i == 1:
+        print(" " * (n-i) + "*")
+    else:
+        print(" " * (n-i) + "*" + "*" * (2*i - 2))'''
+
+        
+
+
+
+
+    
+    
