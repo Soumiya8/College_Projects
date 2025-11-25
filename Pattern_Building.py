9c2a357aa27d0c2910ada1af1876e01a8bdeec52
Soumiya S <soumiyasankar4s@gmail.com>
2025-11-25 22:42:51 +0530

    updated pattern building

--- Pattern_Building.py
+++ Pattern_Building.py
@@ -19,7 +19,19 @@ for i in range (n-1,0,-1) :
         print(" " * (n-i) + "*" + "*" * (2*i - 2))'''
 
         
-
+#Hollow diamond pattern
+'''
+n = int(input("Enter the no. of rows: "))
+for i in range (1,n+1):
+    if i == 1:
+        print(" "*(n-1) + "*")
+    else:
+        print(" "*(n-i) + "*" + " "*(2*i-3) + "*")
+for i in range (n-1,0,-1):
+    if i == 1:
+        print(" "*(n-1) + "*")
+    else:
+        print(" "*(n-i) + "*" + " "*(2*i-3) + "*")'''
 
 
 
