6fb1dd3883c9516214440f8af68717d8fb6fb079
Soumiya S <soumiyasankar4s@gmail.com>
2025-12-18 19:04:40 +0530

    practised file handling

---
+++ phone_contact_file.py
@@ -0,0 +1,44 @@
+#phone contacts file handling
+def add_contact():
+    name = input("enter the name: ")
+    phone = input("enter the phone number: ")
+    with open("contact.txt","a") as f:
+        f.write(name + "-" + phone + "\n")
+
+def view_contacts():
+    try:
+        with open("contact.txt",r):
+            print(f.read())
+    except:
+        print("File not Found")
+
+def search_contact():
+    name = input("Enter the name")
+    try:
+        with open("contact.txt","r") as f:
+            for line in f:
+                if line.startswith(name):
+                    print(line)
+    except:
+        print("File error")
+
+while True:
+    print("\nPhone Book")
+    print("1. Add Contact")
+    print("2. View Contacts")
+    print("3. Search Contact")
+    print("4. Exit")
+    
+    choice = input("Enter your choice: ")
+    
+    if choice == "1":
+        add_contact()
+    elif choice == "2":
+        view_contacts()
+    elif choice == "3":
+        search_contact()
+    elif choice == "4":
+        break
+    else:
+        print("Invalid choice. Please try again.") 
+        
